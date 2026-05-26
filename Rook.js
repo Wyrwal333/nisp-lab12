b34ec94554903ef89703ba3779ec8fa91f93a31a
Jan Kowalski <jakismail@com>
2026-05-26 08:42:12 +0200

    Dodanie Rook.js

--- Rook.js
+++ Rook.js
@@ -5,8 +5,17 @@ class Rook {
     }
 
     validateMove(move) {
-        
-        // dokoncz implementacje
+
+        const dx = Math.abs(move.to.x - move.from.x);
+        const dy = Math.abs(move.to.y - move.from.y);
+
+        // ruch pionowy lub poziomy
+        if ((dx > 0 && dy === 0) || (dy > 0 && dx === 0)) {
+            return true;
+        }
+
         return false;
     }
 }
+
+export default Rook;
